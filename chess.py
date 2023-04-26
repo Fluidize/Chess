eb848bc004d6d59eedcba194a139e76e7223b1c2
Fluidize <mbxiao1@gmail.com>
2023-04-25 21:32:58 -0500

    modifier system

--- chess.py
+++ chess.py
@@ -11,38 +11,39 @@ class Chess:
       board.append(smallarr)
 
     self.board = board
-  
+
   def changeSquare(self, value, new_value="__", type="square"):
     #without specifying new value, square will be reset
     if type == "square":
-        coordinate = ()
-        for z in range(len(self.board)):
-            for i in range(len(self.board[z])):
-                if value in self.board[z][i]:
-                    coordinate = (z, i)
-        piece = self.board[coordinate[0]][coordinate[1]][:2]
-        self.board[coordinate[0]][coordinate[1]] = self.board[coordinate[0]][coordinate[1]].replace(piece, new_value)
+      coordinate = ()
+      for z in range(len(self.board)):
+        for i in range(len(self.board[z])):
+          if value in self.board[z][i]:
+            coordinate = (z, i)
+      piece = self.board[coordinate[0]][coordinate[1]][:2]
+      self.board[coordinate[0]][coordinate[1]] = self.board[coordinate[0]][
+        coordinate[1]].replace(piece, new_value)
     elif type == "coordinate":
-      self.board[value[0]][value[1]] = value.replace("__",new_value)
-    
-  
+      self.board[value[0]][value[1]] = value.replace("__", new_value)
+
   def getSquarePiece(self, value, type="square"):
+    #arr[0] = color and piece
+    #arr[1] = coordinate of piece
+    #arr[2] = square notation
     if type == "square":
-        coordinate = ()
-        print(value)
-        for z in range(len(self.board)):
-            for i in range(len(self.board[z])):
-                print(value , self.board[z][i])
-                if value in self.board[z][i]:
-                    coordinate = (z, i)
-        square = self.board[coordinate[0]][coordinate[1]]
-        return [square[:2], coordinate, square[-2:]]
+      coordinate = ()
+      for z in range(len(self.board)):
+        for i in range(len(self.board[z])):
+          if value in self.board[z][i]:
+            coordinate = (z, i)
+      square = self.board[coordinate[0]][coordinate[1]]
+      return [square[:2], coordinate, square[-2:]]
     elif type == "coordinate":
-       square = self.board[value[0]][value[1]]
-       return [square[:2], value, square[-2:]]
-  
+      square = self.board[value[0]][value[1]]
+      return [square[:2], value, square[-2:]]
+
   def setup(self):
-    back_rank = ["R","N","B","Q","K","B","N","R"]
+    back_rank = ["R", "N", "B", "Q", "K", "B", "N", "R"]
     for rank in self.board:
       for square in rank:
         if "2" in square:
@@ -57,26 +58,48 @@ class Chess:
       else:
         continue
       for z in range(len(rank)):
-          self.changeSquare(rank[z], color + back_rank[z])
-  
+        self.changeSquare(rank[z], color + back_rank[z])
+
   def move(self):
     current_square = input("Square: ")
     next_square = input("Move to: ")
     cur_piece, cur_coordinate, sqr = self.getSquarePiece(current_square)
     possible_moves = []
-    print(cur_piece,cur_coordinate,current_square)
+    print(cur_piece, cur_coordinate, sqr)
     if cur_piece[0] == "w":
       color = "white"
+
+      #king
+      
+      #pawn
       if cur_piece[1] == "P":
-        possible_moves = [self.getSquarePiece((cur_coordinate[0]-1,cur_coordinate[1]), "coordinate")[2], self.getSquarePiece((cur_coordinate[0]-2,cur_coordinate[1]), "coordinate")[2]]
-        if next_square in possible_moves:
-           self.changeSquare(current_square)
-           self.changeSquare(next_square, cur_piece)
+        modifiers = [(-1,0),(-2,0)]
+        for modifier in modifiers:
+          square = self.getSquarePiece((cur_coordinate[0] + modifier[0], cur_coordinate[1] + modifier[1]), "coordinate")
+          if square[0] == "__":
+            possible_moves.append(square)
+        modifiers = [(-1,-1),(-1,1)]
+        for modifier in modifiers:
+          square = self.getSquarePiece((cur_coordinate[0] + modifier[0], cur_coordinate[1] + modifier[1]), "coordinate")
+          if "b" in square[0]:
+            possible_moves.append(square)
+      if cur_piece[1] == "N":
+        modifiers = [(-1,-2),(1,-2),(-2,-1),(-2,1),(2,-1),(2,1),(-1,2),(1,2)]
+        for modifier in modifiers:
+          square = self.getSquarePiece((cur_coordinate[0] + modifier[0], cur_coordinate[1] + modifier[1]), "coordinate")
+          possible_moves.append(square)
+
     elif cur_piece[0] == "b":
       color = "black"
       pass
-    
-    
+    print(possible_moves)
+
+    if next_square in possible_moves:
+      self.changeSquare(current_square)
+      self.changeSquare(next_square, cur_piece)
+    elif next_square not in possible_moves:
+      raise Exception("Illegal Move")
+
     
   
   def displayBoard(self):
@@ -84,7 +107,6 @@ class Chess:
       for square in rank:
         print(square, end="  ")
       print("\n")
-  
 
 
 instance = Chess()
